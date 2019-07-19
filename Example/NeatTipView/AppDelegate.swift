13083e876cd1ee1889bc2bd2de90818212716717
Germán Stábile <german.stabile@gmail.com>
2019-07-19 13:50:46 -0300

    create example app and lib structure

---
+++ Example/NeatTipView/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  NeatTipView
+//
+//  Created by german.stabile@gmail.com on 07/19/2019.
+//  Copyright (c) 2019 german.stabile@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
